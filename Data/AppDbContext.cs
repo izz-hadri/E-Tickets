510493d83bd245397c9c475d12262c960eb9523f
izz-hadri <hadriizz.work@gmail.com>
2022-07-12 21:20:59 +0800

    Setup Database (Code first)

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -29,7 +29,7 @@ namespace eTickets.Data
         }
 
         public DbSet<Actor> Actors { get; set; }
-        public DbSet<Movie> Movies{ get; set; }
+        public DbSet<Movie> Movies { get; set; }
         public DbSet<Actor_Movie> Actor_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
