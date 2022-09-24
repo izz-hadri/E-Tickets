ea67b87b7ddc27ec66a97ba61065631d63464627
izz-hadri <hadriizz.work@gmail.com>
2022-09-25 00:03:44 +0800

    post create actor with model validation bug fix on actor movies by set it to nullable

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -23,5 +23,18 @@ namespace eTickets.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+
+            _service.Add(actor);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- Models/Actor.cs
+++ Models/Actor.cs
@@ -8,14 +8,18 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required.")]
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Full name must be between and 50 characters.")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required.")]
         public string Bio { get; set; }
 
-        public List<Actors_Movies> Actors_Movies { get; set; }
+        public List<Actors_Movies>? Actors_Movies { get; set; }
     }
 }
