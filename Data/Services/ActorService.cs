61b6a4d913afb6a54332baf6fef3e58171f1e2ea
izz-hadri <hadriizz.work@gmail.com>
2022-09-25 15:00:49 +0800

    Implement generic method for entity repository

--- Data/Services/ActorService.cs
+++ Data/Services/ActorService.cs
@@ -1,61 +1,15 @@
-﻿using eTickets.Data.Services.Interfaces;
+﻿using eTickets.Data.Base;
+using eTickets.Data.Services.Interfaces;
 using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Data.Services
 {
-    public class ActorService : IActorsService
+    public class ActorService : EntityBaseRepository<Actor>, IActorsService 
     {
-        private readonly AppDbContext _context;
-
-        public ActorService(AppDbContext context)
+        public ActorService(AppDbContext context): base(context)
         {
-            _context = context;
-        }
 
-        public async Task AddAsync(Actor actor)
-        {
-            _context.Actors.AddAsync(actor);
-            
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            Actor result = await _context.Actors.FirstOrDefaultAsync(x => x.Id == id);
-
-            _context.Actors.Remove(result);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            List<Actor> result = await _context.Actors.ToListAsync();
-
-            return result;
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
-        {
-            Actor result = await _context.Actors.FirstOrDefaultAsync(x => x.Id == id);
-            
-            return result;
-        }
-
-        public async Task<Actor> UpdateAsync(int id, Actor newActor)
-        {
-            _context.Update(newActor);
-
-            try 
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            return newActor;
         }
     }
 }
